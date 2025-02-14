c914657c264b8b413ad875435f3be537a2529032
Jerome <nietsemorej@gmail.com>
2025-02-14 15:14:21 -0800

    fix: export StatusPage

---
+++ src/app/(frontend)/(aet-app)/status/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+import FCEForm from '../components/FCE-Form'
+
+export const metadata: Metadata = {
+  title: 'AET Service Application Status',
+  description: 'AET Service Application Status',
+}
+
+export default function StatusPage() {
+  return <div className="container mx-auto py-10"></div>
+}
